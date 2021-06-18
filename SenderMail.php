0d04e34097387e6c01da92d824ac871420f86e9b
Yuts-T34 <68275906+Yuts-T34@users.noreply.github.com>
2021-06-19 00:56:02 +0700

    Update SenderMail.php

--- SenderMail.php
+++ SenderMail.php
@@ -7,7 +7,7 @@
     $to = "thirayutsae717@gmail.com";
     $subject = "Mail From website";
     $txt = "Name = " . $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
-    $headers = "From: noreply@yuts-t34.github.io/teerayuts-wedsite.com" . "\r\n" .
+    $headers = "From: noreply@teerayuts-wedsite.com" . "\r\n" .
         "CC: somebodyelse@example.com";
     if ($email != NULL) {
         mail($to, $subject, $txt, $headers);
