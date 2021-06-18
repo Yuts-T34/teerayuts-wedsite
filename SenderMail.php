62254e7307de2864f5822b951e98c92df81190b9
Yuts-T34 <68275906+Yuts-T34@users.noreply.github.com>
2021-06-19 00:39:00 +0700

    Update SenderMail.php

--- SenderMail.php
+++ SenderMail.php
@@ -4,7 +4,7 @@
     $name = $_POST['name'];
     $email = $_POST['email'];
     $message = $_POST['message'];
-    $to = "thirayutsae717@mail.com";
+    $to = "thirayutsae717@gmail.com";
     $subject = "Mail From website";
     $txt = "Name = " . $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
     $headers = "From: noreply@teerayut-wedsite.com" . "\r\n" .
